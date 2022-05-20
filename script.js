a805b055fbc78df4a5d9dac2ce729066f98ae73a
Neel-Patel068 <patelneelh1910@gmail.com>
2022-05-20 22:42:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+matching the designs - it does NOT need to be pixel perfect
+make clicks work on the item
+make arrow key ( up/down ) navigation work
+handle labels that do NOT fit into the given dimensions
+the title in the right section should be editable. no need to retain it on refresh.
+the entire layout should stay in the center
+create a github repo ( you can use your existing github user OR you can create a new user using sprinklr email and mark the repo as private. add chinmaychaudharyspr as a collaborator )
+make incremental commits and push them to the repo
+make github page to host the app ( https://pages.github.com/ read for "Project site" )
+
+*/
+
