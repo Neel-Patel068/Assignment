6269e5efc867eedfb3db0235cc83a72359061216
Neel-Patel068 <patelneelh1910@gmail.com>
2022-05-22 11:58:14 +0530

    Comments added and CSS updated

--- script.js
+++ script.js
@@ -1,82 +1,38 @@
-/*
-matching the designs - it does NOT need to be pixel perfect
-make clicks work on the item
-make arrow key ( up/down ) navigation work
-handle labels that do NOT fit into the given dimensions
-the title in the right section should be editable. no need to retain it on refresh.
-the entire layout should stay in the center
-create a github repo ( you can use your existing github user OR you can create a new user using sprinklr email and mark the repo as private. add chinmaychaudharyspr as a collaborator )
-make incremental commits and push them to the repo
-make github page to host the app ( https://pages.github.com/ read for "Project site" )
-
-Note that the width of the left column should always be 300px 
-
-*/
-
-// global variables
-// console.log(json);
+// allitems contains the image and title of image 
+// we fetch data using fetch function from items.json file
 var allitems=[];
 fetch('./items.json')
   .then(response => response.json())
   .then((data )=> 
   {
-    //   console.log(data);
       allitems=data;
       updateLeft();
       updateRight();
   })
   .catch(error => console.log(error));
 
-/*
-const allitems = [
-    {
-        "previewImage": "https://images.unsplash.com/photo-1561948955-570b270e7c36?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
-        "title": "cat.jpeg"
-    },
-    {
-        "previewImage": "https://images.unsplash.com/photo-1606787620819-8bdf0c44c293?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
-        "title": "a man and a woman trying to cook a meal together in a modern kitchen.jpg"
-    },
-    {
-        "previewImage": "https://images.unsplash.com/photo-1539367628448-4bc5c9d171c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
-        "title": "bali-kelingking-beach-plastic-removal-drive.key"
-    },
-    {
-        "previewImage": "https://images.unsplash.com/photo-1623206837956-07dab21608f6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
-        "title": "NextByk Investor Pitch 2022.ppt"
-    },
-    {
-        "previewImage": "https://images.unsplash.com/photo-1526628953301-3e589a6a8b74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
-        "title": "interns-performance-report-may-2022.key"
-    }
-];
-*/
 
-const left = document.querySelector(".left");
-const right = document.querySelector(".right");
-var currentlySelectedImg = 0;
-var previouslySelectedImg = 0;
+const left = document.querySelector(".left");     // left component which contains the unorderd list 
+const right = document.querySelector(".right");   //  right component which contains the image and caption 
+var currentlySelectedImg = 0;                   // currentky selected image index
+var previouslySelectedImg = 0;                  // previously selected image index
 
+// this function cut the title text if it is larger than max possible length 
 const cropTitle = function(title) {
-    const maxPossibleLength = 27;
+    const maxPossibleLength = 20;
     if(title.length<= maxPossibleLength ) 
-    {
         return title;
-    }
     else 
-    {
-        let updatedTitle= title.slice(0,13)+"..." + title.slice(-14);
-        return updatedTitle;
-    }
+        return title.slice(0,10)+"..." + title.slice(-10);;
 }
 
+// This function will update the left column
 const updateLeft = function () {
     const unorderdList = document.querySelector("ul");
     unorderdList.innerHTML = "";
     allitems.forEach((item,index)=>{
         const row= document.createElement("li");
-        row.innerHTML = `<img src=${allitems[index].previewImage} id=${index} class="crop"> <span> ${cropTitle(allitems[index].title)}</span>`;
-        // console.log(cropTitle(allitems[index].title));
+        row.innerHTML = `<img src=${allitems[index].previewImage} id=${"id"+index} class="crop"> <span> ${cropTitle(allitems[index].title)}</span>`;
         unorderdList.append(row);
         row.addEventListener("click",()=>{
             previouslySelectedImg = currentlySelectedImg;
@@ -84,9 +40,9 @@ const updateLeft = function () {
             updateRight();
         });
     });
-    left.append(unorderdList);
 }
 
+// This function will update the right column
 const updateRight = function () {
     heightLightLeft();
     const rightImg = right.querySelector(".bigImg");
@@ -96,40 +52,28 @@ const updateRight = function () {
     rightTitle.innerText = allitems[currentlySelectedImg].title;
     rightTitle.addEventListener("blur",()=>{
         allitems[currentlySelectedImg].title = rightTitle.innerHTML;
-        // console.log("Blur clicked.", previouslySelectedImg,currentlySelectedImg);
-        
         updateLeft();
         heightLightLeft();
     });
 }
 
-
-
-
-// keyboard eventListener
-
+// keyboard eventListener detects the arrow up and down, and response accordingly
 window.addEventListener("keydown",(event)=>{
-    if(event.key == "ArrowDown")
+    if(event.key == "ArrowDown" && (currentlySelectedImg+1 < allitems.length) )
     {
-        if(currentlySelectedImg+1 < allitems.length)
-        {
-            previouslySelectedImg = currentlySelectedImg;
-            currentlySelectedImg++;
-            updateRight();
-        }
+        previouslySelectedImg = currentlySelectedImg;
+        currentlySelectedImg++;
+        updateRight();
     }
-    else if(event.key == "ArrowUp")
+    else if(event.key == "ArrowUp" && (currentlySelectedImg -1 >= 0) )
     {
-        if(currentlySelectedImg -1 >= 0)
-        {
-            previouslySelectedImg = currentlySelectedImg;
-            currentlySelectedImg--;
-            updateRight();
-        }
+        previouslySelectedImg = currentlySelectedImg;
+        currentlySelectedImg--;
+        updateRight();
     }
 });
 
-const heightLightLeft = function (){
+const heightLightLeft = function () {
     document.querySelectorAll("li")[previouslySelectedImg].classList.remove("active");
     document.querySelectorAll("li")[currentlySelectedImg].classList.add("active");
 }
